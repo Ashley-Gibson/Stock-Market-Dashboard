e1fbd4301d44215a1282a28eb39f01608d58415a
Ashley Gibson <ash.gibson@hotmail.co.uk>
2020-03-29 13:36:43 +0100

    master - Initial Commit for project

---
+++ Stock Market Dashboard/Stock Market Dashboard/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Stock_Market_Dashboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
