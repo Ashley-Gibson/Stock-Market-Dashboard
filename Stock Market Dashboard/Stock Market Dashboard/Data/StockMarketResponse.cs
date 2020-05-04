819c3c67044f48daa7b322ad5ade22b185578a65
Ashley Gibson <ash.gibson@hotmail.co.uk>
2020-05-04 23:41:56 +0100

    tasK/SMD-159 - Displaying the amount of closed prices divided by 100 to the graph but need to do the correct dates to match this

--- Stock Market Dashboard/Stock Market Dashboard/Data/StockMarketResponse.cs
+++ Stock Market Dashboard/Stock Market Dashboard/Data/StockMarketResponse.cs
@@ -2,10 +2,12 @@ namespace Stock_Market_Dashboard.Data
 {
     public class StockMarketResponse
     {        
-        public double o { get; set; } // Open price for the day
-        public double h { get; set; } // High price for the day
-        public double l { get; set; } // Low price for the day
-        public double c { get; set; } // Current price
-        public double pc { get; set; } // Previous close price
+        public double[] o { get; set; } // List of open prices for returned candles
+        public double[] h { get; set; } // List of high prices for returned candles
+        public double[] l { get; set; } // List of low prices for returned candles
+        public double[] c { get; set; } // List of close prices for returned candles
+        public double[] v { get; set; } // List of volume data for returned candles
+        public long[] t { get; set; } // List of timestamps for returned candles
+        public string s { get; set; } // Status of the response. This field can either be ok or no_data.
     }
 }
--- Stock Market Dashboard/Stock Market Dashboard/Data/StockMarketService.cs
+++ Stock Market Dashboard/Stock Market Dashboard/Data/StockMarketService.cs
@@ -8,23 +8,38 @@ namespace Stock_Market_Dashboard.Data
     public class StockMarketService
     {
         private string APIKey => "bqdkitnrh5re4rmf4580";
-        private  string QuoteWebhook => "https://finnhub.io/api/v1/quote";
+        private string Webhook => "https://finnhub.io/api/v1/stock/candle";
 
-        private  HttpClient client = new HttpClient();
+        private readonly HttpClient client = new HttpClient();
 
         public StockMarketService()
         {
             client.Timeout = TimeSpan.FromSeconds(10);
         }
 
-        private  string BuildWebhookPath(string company = "AAPL") 
+        private string BuildWebhookPath(string companyCode, DateTime startDate, DateTime endDate) 
         {
-            return QuoteWebhook + "?symbol=" + company + "&token=" + APIKey;
+            long startUnixTimestamp = ConvertDateToUnix(startDate);
+            long endUnixTimestamp = ConvertDateToUnix(endDate);
+            
+            return Webhook + "?symbol=" + companyCode + "&from=" + startUnixTimestamp + "&to=" + endUnixTimestamp + "&resolution=1&token=" + APIKey;
         }
 
-        public async Task<StockMarketResponse> GetStockMarketDataForCompanyAsync(string company = "AAPL")
+        private long ConvertDateToUnix(DateTime date)
         {
-            string path = BuildWebhookPath(company);
+            TimeSpan unixDate = date - new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc); 
+
+            return (long)unixDate.TotalSeconds;
+        }
+
+        public DateTime ConvertUnixToDate(long unixTimestamp)
+        {
+            return new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc).AddSeconds(unixTimestamp);
+        }
+
+        public async Task<StockMarketResponse> GetStockMarketDataForCompanyAsync(string companyCode, DateTime startDate, DateTime endDate)
+        {
+            string path = BuildWebhookPath(companyCode, startDate, endDate);
             StockMarketResponse response = await client.GetJsonAsync<StockMarketResponse>(path).ConfigureAwait(continueOnCapturedContext:false);
             return response;
         }
