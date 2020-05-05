3a14a94394908333f48a76d082d871f5438a022a
Ashley Gibson <ash.gibson@hotmail.co.uk>
2020-05-05 18:37:05 +0100

    task/SMD-165 - Pulled out functions to Extensions project and added 2 decimal places restriction to output numbers

--- Stock Market Dashboard/Stock Market Dashboard/Data/StockMarketService.cs
+++ Stock Market Dashboard/Stock Market Dashboard/Data/StockMarketService.cs
@@ -1,3 +1,4 @@
+using Extensions;
 using Microsoft.AspNetCore.Components;
 using System;
 using System.Net.Http;
@@ -19,22 +20,10 @@ namespace Stock_Market_Dashboard.Data
 
         private string BuildWebhookPath(string companyCode, DateTime startDate, DateTime endDate) 
         {
-            long startUnixTimestamp = ConvertDateToUnix(startDate);
-            long endUnixTimestamp = ConvertDateToUnix(endDate);
+            long startUnixTimestamp = Conversions.ConvertDateToUnix(startDate);
+            long endUnixTimestamp = Conversions.ConvertDateToUnix(endDate);
             
             return Webhook + "?symbol=" + companyCode + "&from=" + startUnixTimestamp + "&to=" + endUnixTimestamp + "&resolution=D&token=" + APIKey;
-        }
-
-        private long ConvertDateToUnix(DateTime date)
-        {
-            TimeSpan unixDate = date - new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc); 
-
-            return (long)unixDate.TotalSeconds;
-        }
-
-        public DateTime ConvertUnixToDate(long unixTimestamp)
-        {
-            return new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc).AddSeconds(unixTimestamp);
         }
 
         public async Task<StockMarketResponse> GetStockMarketDataForCompanyAsync(string companyCode, DateTime startDate, DateTime endDate)
