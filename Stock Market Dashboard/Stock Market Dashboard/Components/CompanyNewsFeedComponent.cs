897085ee260420a4cfaa5473962155e6ccf6f903
Ashley Gibson <ash.gibson@hotmail.co.uk>
2020-06-27 18:14:26 +0100

    task/SMD-175 - Improved Publish Profile | Implemented better Components and begun building Tabs for the Company News Feed

---
+++ Stock Market Dashboard/Stock Market Dashboard/Components/CompanyNewsFeedComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Stock_Market_Dashboard.Components
+{
+    public partial class CompanyNewsFeedComponentBase : ComponentBase
+    {
+        protected string selectedTab = "Amazon";
+        protected void OnSelectedTabChanged(string name)
+        {
+            selectedTab = name;
+        }
+    }
+}
---
+++ Stock Market Dashboard/Stock Market Dashboard/Components/DataSettingsComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Stock_Market_Dashboard.Data;
+using Extensions;
+using Blazorise.Charts;
+
+namespace Stock_Market_Dashboard.Components
+{
+    public partial class DataSettingsComponentBase : ComponentBase
+    {
+
+    }
+}
